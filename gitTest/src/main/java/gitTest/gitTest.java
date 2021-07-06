44ee21ecf319e9952d6405c8f503423d36023494
A <A@DESKTOP-DCCUH1M>
2021-07-06 16:50:43 +0900

    Create Test02

--- gitTest/src/main/java/gitTest/gitTest.java
+++ gitTest/src/main/java/gitTest/gitTest.java
@@ -3,7 +3,8 @@ package gitTest;
 public class gitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Test01");
+		System.out.println("Test02");
 
 	}
 
